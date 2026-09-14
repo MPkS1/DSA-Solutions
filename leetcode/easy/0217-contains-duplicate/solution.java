e2beff315155060b41eb1a40908010efeb7de551
M Purushottama Kumar <163711651+MPkS1@users.noreply.github.com>
2026-09-14 11:04:31 +0530

    ✅ Contains Duplicate | Easy | Java | Runtime: 35 ms (5.23%) | Memory: 95.6 MB | 2026-09-14

--- leetcode/easy/0217-contains-duplicate/solution.java
+++ leetcode/easy/0217-contains-duplicate/solution.java
@@ -1,10 +1,5 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> hs=new HashSet<>();
-        for(int i:nums)
-        {
-            hs.add(i);
-        }
-        return hs.size()!=nums.length;
+        return Arrays.stream(nums).distinct().count()!=nums.length;
     }
 }
