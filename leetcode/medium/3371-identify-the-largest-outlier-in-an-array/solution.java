d772a3c319fa3730d84949e28a42277ee46d4e1c
M Purushottama Kumar <163711651+MPkS1@users.noreply.github.com>
2026-09-15 21:55:58 +0530

    ✅ Identify the Largest Outlier in an Array | Medium | Java | Runtime: 84 ms (77.64%) | Memory: 114.7 MB | 2026-09-15

--- leetcode/medium/3371-identify-the-largest-outlier-in-an-array/solution.java
+++ leetcode/medium/3371-identify-the-largest-outlier-in-an-array/solution.java
@@ -1,25 +1,22 @@
 class Solution {
     public int getLargestOutlier(int[] nums) {
-        int total = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            total += num;
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        int t=0;
+        for(int n:nums)
+        {
+            t+=n;
+            hm.put(n,hm.getOrDefault(n,0)+1);
         }
-
-        int answer = Integer.MIN_VALUE;
-
-        for (int num : nums) {
-            int outlier = total - 2 * num;
-
-            if (map.containsKey(outlier)) {
-                if (outlier != num || map.get(outlier) > 1) {
-                    answer = Math.max(answer, outlier);
-                }
+        int ans=Integer.MIN_VALUE;
+        for(int n:nums)
+        {
+            int outl=t-2*n;
+            if(hm.containsKey(outl))
+            {
+                if(outl!=n||hm.get(n)>1)
+                    ans=Math.max(outl,ans);
             }
         }
-
-        return answer;
+        return ans;
     }
 }
