03ad6c8ed1a82bffe9ed8596ce70c5003e2dce7a
M Purushottama Kumar <163711651+MPkS1@users.noreply.github.com>
2026-09-20 12:21:45 +0530

    ✅ Maximum Average Subarray I | Easy | Java | Runtime: 0 ms | Memory: 45 MB | 2026-09-20

---
+++ leetcode/easy/0643-maximum-average-subarray-i/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double max=0;
+        int i=0,j=k-1,sum=0;
+        for(int l=0;l<k;l++)
+            sum+=nums[l];
+        max=sum/k;
+        System.out.println(max);
+        while(j<nums.length-1)
+        {
+            sum-=nums[i++];
+            sum+=nums[++j];
+            if(sum/k>max)
+            {
+                 max=sum/k;
+                System.out.println(max);
+            }   
+            
+        }
+        return max;
+    }
+}
