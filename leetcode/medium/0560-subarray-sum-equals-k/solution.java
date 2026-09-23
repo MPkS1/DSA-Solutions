e609e5169d24a4c34460f023c8311f17d14f006e
M Purushottama Kumar <163711651+MPkS1@users.noreply.github.com>
2026-09-23 22:15:00 +0530

    ✅ Subarray Sum Equals K | Medium | Java | Runtime: 23 ms (92.80%) | Memory: 48.8 MB | 2026-09-23

---
+++ leetcode/medium/0560-subarray-sum-equals-k/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        int sum=0,count=0;
+        hm.put(0,1);
+        for(int i:nums)
+        {
+            sum+=i;
+            if(hm.containsKey(sum-k))
+            {
+                count+=hm.get(sum-k);
+            }
+            hm.put(sum,hm.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
