55e82dd8bba640165d68aefbb1bc13437c37ea71
M Purushottama Kumar <163711651+MPkS1@users.noreply.github.com>
2026-09-24 15:17:09 +0530

    ✅ Rotate Array | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ gfg/medium/rotate-array-by-n-elements-1587115621/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotateArr(int[] arr, int d) {
+        int n=arr.length;
+        d=d%n;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+    }
+    public void reverse(int[] arr,int l,int r) {
+        while(l<r) {
+            int t=arr[l];
+            arr[l]=arr[r];
+            arr[r]=t;
+            l++;
+            r--;
+        }
+    }
+}
