01e6f99ce0530db326c1df447e801382f02850bb
M Purushottama Kumar <163711651+MPkS1@users.noreply.github.com>
2026-09-24 07:02:43 +0530

    ✅ Subarray Sums Divisible by K | Medium | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-09-24

---
+++ leetcode/medium/0974-subarray-sums-divisible-by-k/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        hm.put(0,1);
+        int sum=0,count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=nums[i];
+            int rem=sum%k;
+            if(hm.containsKey(rem))
+            {
+                count+=hm.get(rem);
+            }
+            hm.put(rem,hm.getOrDefault(rem,1)+1);
+        }
+        return count;
+    }
+}
